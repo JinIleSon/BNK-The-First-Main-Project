b37c8867c4a646364a245805d5b3a7f8dd2a70af
rainbow0oo0 <eternalsoul0634@gmail.com>
2025-11-10 12:34:21 +0900

    Pakege create & html header+footer 수정 & button 공통사용

---
+++ BNK-RetirementPension/BNKFirst/src/main/java/kr/co/bnkfirst/dto/UsersDTO.java
@@ -0,0 +1,36 @@
+package kr.co.bnkfirst.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class UsersDTO {
+
+    private String uId;
+    private String mId;
+    private String mPw;
+    private String mName;
+    private String mBirth;
+    private String mGender;
+    private String mCarrier;
+    private String mEmail;
+    private String mPhone;
+    private String mDate;
+    private String mGrade;
+    private String mJumin;
+    private String mCond;
+    private String mNum;
+    private String mAccess;
+    private String mLimit;
+
+    /*
+        시간 남으면 DB 추가 후 출력 예정
+        진행시 패널 형식x → 각 약관을 버튼으로 바꾼 후 누르면 모달창 형식으로 변경
+        ↓ 원래 하드 코딩으로 예정
+     */
+    private String mTitle;
+    private String mContent;
+}
