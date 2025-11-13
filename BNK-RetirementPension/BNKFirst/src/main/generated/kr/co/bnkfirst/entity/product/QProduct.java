25e322fd37f2e1c0671a60a61005329d494f9e55
mincheolkang34 <mincheolkang34@gmail.com>
2025-11-13 12:49:45 +0900

    상품 상세 구현 중

--- BNK-RetirementPension/BNKFirst/src/main/generated/kr/co/bnkfirst/entity/product/QProduct.java
+++ BNK-RetirementPension/BNKFirst/src/main/generated/kr/co/bnkfirst/entity/product/QProduct.java
@@ -27,6 +27,8 @@ public class QProduct extends EntityPathBase<Product> {
 
     public final StringPath pcprd = createString("pcprd");
 
+    public final StringPath pdirate = createString("pdirate");
+
     public final StringPath pelgbl = createString("pelgbl");
 
     public final NumberPath<Float> phirate = createNumber("phirate", Float.class);
@@ -43,9 +45,7 @@ public class QProduct extends EntityPathBase<Product> {
 
     public final StringPath prmthd = createString("prmthd");
 
-    public final StringPath ptlink = createString("ptlink");
-
-    public final StringPath pttitle = createString("pttitle");
+    public final StringPath pterms = createString("pterms");
 
     public final StringPath ptype = createString("ptype");
 
