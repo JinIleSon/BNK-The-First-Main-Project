d50d8c144ff5235cb6a104ca5ac9b31a9f91890a
yingbbang <yingbbang@naver.com>
2025-11-21 11:12:54 +0900

    Update retirement controller, security config, and header layout

--- BNK-RetirementPension/BNKFirst/src/main/java/kr/co/bnkfirst/controller/RetirementInfoController.java
+++ BNK-RetirementPension/BNKFirst/src/main/java/kr/co/bnkfirst/controller/RetirementInfoController.java
@@ -8,60 +8,6 @@ public class RetirementInfoController {
 
     @GetMapping("/retirement-renew/intro/why-pension")
     public String whyPension(){
-        return "retirement-renew/intro/why-pension";
+        return "retirement-renew/intro/why-pension/why-pension";
     }
-
-    @GetMapping("/info/guide/coreValues")
-    public String coreValue() {
-        return "info/guide/coreValues/info_coreValues";
-    }
-
-    @GetMapping("/info/guide/groupMission")
-    public String groupMission() {
-        return "info/guide/groupMission/info_groupMission";
-    }
-
-    @GetMapping("/info/guide/groupVision")
-    public String groupVision() {
-        return "info/guide/groupVision/info_groupVision";
-    }
-
-    @GetMapping("/info/guide/missionStrategy")
-    public String missionStrategy() {
-        return "info/guide/missionStrategy/info_missionStrategy";
-    }
-
-    @GetMapping("/info/guide/strategySlogan")
-    public String strategySlogan() {
-        return "info/guide/strategySlogan/info_strategySlogan";
-    }
-
-    @GetMapping("/info/guide")
-    public String guideIndex() {
-        return "info/index";
-    }
-
-    @GetMapping("/info/story")
-    public String storyIndex() {return  "info/story/info_story";}
-
-    @GetMapping("/board/press")
-    public String pressIndex() {return  "info/board/storyPress/list";}
-
-    @GetMapping("/board/notice")
-    public String NoticeIndex() {return  "info/board/storyNotice/list";}
-
-    @GetMapping("/board/event")
-    public String eventIndex() {return  "info/board/storyEvent/list";}
-
-    @GetMapping("/board/news")
-    public String newsIndex() {return  "info/board/storyNews/list";}
-
-    @GetMapping("/board/media")
-    public String mediaIndex() {return  "info/board/storyMedia/list";}
-
-    @GetMapping("/info/invest")
-    public String investIndex() {return  "info/invest/info_invest";}
-
-    @GetMapping("/info/branch")
-    public String branchIndex() {return  "info/branch/info_branch";}
 }
