f94ed64e21f347b058578d937f50100c7b4f64c1
mincheolkang34 <mincheolkang34@gmail.com>
2025-11-24 15:59:26 +0900

    상품 정보입력 4단계 구현 중

--- BNK-RetirementPension/BNKFirst/src/main/resources/static/js/product/fill_prod_info.js
+++ BNK-RetirementPension/BNKFirst/src/main/resources/static/js/product/fill_prod_info.js
@@ -1,13 +1,22 @@
-const $ = (sel, root = document) => root.querySelectorAll(sel);
+const $ = (sel, root = document) => root.querySelector(sel);
+const $$ = (sel, root = document) => root.querySelectorAll(sel);
 function fillTerms(pterms) {
-    const termsTitle = [...$('#page2 .terms-list span')];
-    const termsURL = [...$('#page2 .terms-list a')];
+    const termsTitle = [...$$('#page2 .terms-list span')];
+    const termsURL = [...$$('#page2 .terms-list a')];
     for (let i = 0; i < termsTitle.length; i++) {
         termsTitle[i].innerText = pterms[i].title;
         termsURL[i].setAttribute('href', pterms[i].link);
         termsURL[i].setAttribute('target', '_blank');
         termsURL[i].setAttribute('rel', 'noopener noreferrer');
     }
+}
+
+function fillPname(pname) {
+    const page2ProdTitle = $('.product-name');
+    const page4Pname = $('input[name="pname"]');
+
+    page2ProdTitle.innerText = pname;
+    page4Pname.value = pname;
 }
 
 export function initProdInfo(data) {
